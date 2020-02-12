d3d0467ed0d33069e5c3865aafdd5cf4e7a9a4e3
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-02-11 22:07:22 -0800

    third commit

--- interactive-form/js/script.js
+++ interactive-form/js/script.js
@@ -1,8 +1,14 @@
 //global variables
 const userName = document.getElementById('name');
-
-
+const otherJobRole = document.getElementById('other-title');
+const tShirttheme = document.getElementById('design')
+//basic info
 //sets focus to the first text field on page upload
 window.onload = function () {
   userName.focus();
 }
+
+//hides "other" job role input
+otherJobRole.style.display = 'none';
+
+//T-shirt
